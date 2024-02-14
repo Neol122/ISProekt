c5c07ca02613dfcb00511818e6c4515fd4db2d95
Neol122 <33427520+Neol122@users.noreply.github.com>
2024-02-14 12:25:05 +0100

    Add files via upload

---
+++ MovieStoreMvc/MovieStoreMvc/MovieStoreMvc/Controllers/UserAuthenticationController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieStoreMvc.Models.DTO;
+using MovieStoreMvc.Repositories.Abstract;
+
+namespace MovieStoreMvc.Controllers
+{
+    public class UserAuthenticationController : Controller
+    {
+        private IUserAuthenticationService authService;
+        public UserAuthenticationController(IUserAuthenticationService authService)
+        {
+            this.authService = authService;
+        }
+      
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var result = await authService.LoginAsync(model);
+            if (result.StatusCode == 1)
+                return RedirectToAction("Index", "Home");
+            else
+            {
+                TempData["msg"] = "Could not log in..";
+                return RedirectToAction(nameof(Login));
+            }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+           await authService.LogoutAsync();
+            return RedirectToAction(nameof(Login));
+        }
+
+    }
+}
